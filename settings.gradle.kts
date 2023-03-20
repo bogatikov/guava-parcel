d64af0d6efc903609d6173bc630dde6ec26be408
aybogati <aybogati@mts.ru>
2023-03-20 10:49:05 +0300

    Add services structure

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "guava-parcel"
+
+include("guava-parcel-gateway")
+include("guava-parcel-auth")
+include("guava-parcel-delivery")
+include("guava-parcel-admin")
+include("guava-parcel-courier")
