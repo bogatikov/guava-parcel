4c40b4e85498e5a2762cca19b4202060b5e717f9
aybogati <aybogati@mts.ru>
2023-03-20 10:50:57 +0300

    [ADD] Gateway service

---
+++ guava-parcel-gateway/src/main/java/com/guava/guavaparcel/GuavaParcelApplication.java
@@ -0,0 +1,13 @@
+package com.guava.guavaparcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavaParcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuavaParcelApplication.class, args);
+    }
+
+}
